c8cb076cf612391b35bc7ed457450b4e00450c28
Andrew Tedstone <a.j.tedstone@bristol.ac.uk>
2017-02-09 14:27:01 +0000

    bug fix and add metadata to output geotiff

--- allocate_coast_to_basin.py
+++ allocate_coast_to_basin.py
@@ -24,6 +24,7 @@ iho = gpd.read_file('/home/at15963/Dropbox/work/data/World_Seas_IHO_v2/World_Sea
 # Retain only the basins of interest - makes the distance calculations later faster
 in_arctic = iho.intersects(Polygon(((-121, 52), (43, 52), (43, 90), (-121, 90))))
 iho_arctic = iho[in_arctic]
+iho = None
 
 # Get grids of lat/lon coordinates 
 lons, lats = dist_land.coordinates(latlon=True)
@@ -32,8 +33,8 @@ lats_coast = np.where(coast == 1, lats, np.nan)
 lons_coast_pts = lons_coast[~np.isnan(lons_coast)].flatten()
 lats_coast_pts = lats_coast[~np.isnan(lats_coast)].flatten()
 ll_coast_points = np.zeros((len(lons_coast_pts), 2))
-ll_coast_points[:, 0] = lats_coast_pts
-ll_coast_points[:, 1] = lons_coast_pts
+ll_coast_points[:, 0] = lons_coast_pts
+ll_coast_points[:, 1] = lats_coast_pts
 
 # Calculate equivalent pixel coordinates so we can create a raster of outflux basins
 x = np.arange(0, dist_land.nx)
@@ -55,6 +56,13 @@ for (y, x), (lon, lat) in zip(coast_points, ll_coast_points):
 	basins.append(idx)
 	basins_raster[y, x] = idx
 
+basin_desc = ''
+for b in iho_arctic.iterrows():
+	basin_desc += ', %s:%s' % (b[0], b[1]['NAME'])
+
+history = 'Basins are as defined by "Limits of Oceans & Seas, Special Publication No. 23" published by the IHO in 1953. The dataset was composed by the Flanders Marine Data and Information Centre and downloaded from www.marineregions.org on 6 February 2017. Coastal runoff pixels were allocated an ocean basin to run off into using fwf/allocate_coast_to_basin.py.'
+author = 'Andrew Tedstone, University of Bristol'
+metadata = { 'basins':basin_desc, 'history':history, 'author':author }
 
 ## Save basins raster
 georaster.simple_write_geotiff(
@@ -62,7 +70,8 @@ georaster.simple_write_geotiff(
 	basins_raster, 
 	dist_land.trans,
 	proj4=dist_land.srs.ExportToProj4(),
-	dtype=georaster.gdal.GDT_UInt16
+	dtype=georaster.gdal.GDT_UInt16,
+	metadata=metadata
 	)
 
 
